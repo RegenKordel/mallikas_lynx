657e033bbbc3ed757019057e74da05c622b6637c
laurtom <tlaurinen@gmail.com>
2019-04-01 14:42:55 +0300

    swagger fix, db to persistent

--- src/main/java/eu/openreq/mallikas/SwaggerConfig.java
+++ src/main/java/eu/openreq/mallikas/SwaggerConfig.java
@@ -17,6 +17,7 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+        		.host("api.openreq.eu/mallikas")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.openreq.mallikas.controllers"))    
                 .paths(PathSelectors.any())  
