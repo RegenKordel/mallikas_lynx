59abd3f675932a0afb625e67d378c02541e484e8
laurtom <tlaurinen@gmail.com>
2019-04-12 15:17:22 +0300

    deletemapping again

--- src/main/java/eu/openreq/mallikas/controllers/MallikasController.java
+++ src/main/java/eu/openreq/mallikas/controllers/MallikasController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -511,7 +512,7 @@ public class MallikasController {
 	 * @return
 	 */
 	@ApiIgnore
-	@GetMapping(value = "deleteAllButRejectedDependencies")
+	@DeleteMapping(value = "deleteAllButRejectedDependencies")
 	public ResponseEntity<String> deleteAllButRejectedDependencies() {
 		projectRepository.deleteAll();
 		reqRepository.deleteAll();
