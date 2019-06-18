747702e4d2155716245dd66a780fff4a13522e6c
laurtom <tlaurinen@gmail.com>
2019-06-18 19:12:41 +0300

    still trying(2)

--- src/main/java/eu/openreq/mallikas/MallikasApplication.java
+++ src/main/java/eu/openreq/mallikas/MallikasApplication.java
@@ -2,10 +2,6 @@ package eu.openreq.mallikas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class MallikasApplication {
